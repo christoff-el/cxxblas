0494e54b028f780c43116f22b8754a3686abe438
IldarRakhimov <ildar.rakhimov@gmail.com>
2013-03-17 18:47:57 +0100

    Added a nrm2 implementation

---
+++ level1/nrm2.h
@@ -0,0 +1,8 @@
+#ifndef CXXBLAS_LEVEL1_NRM2_H
+#define CXXBLAS_LEVEL1_NRM2_H 1
+
+template <typename IndexType, typename X, typename Num>
+	void
+	nrm2(IndexType n, const X x, IndexType incX, Num &value);
+
+#endif //CXXBLAS_LEVEL_1_NRM2_H 1
