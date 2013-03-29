fb743dd956d9c9cae7da808b020d82a1b3bd20c0
Chris Davis <chrisdavis132@gmail.com>
2013-03-29 20:55:07 +0000

    Added double precision test suite + linked in export file
    Also added complex test suites, but no linking yet..

--- test-suite/export.cpp
+++ test-suite/export.cpp
@@ -15,6 +15,12 @@
 
 extern "C" {
 
+//-----------------------------------------------------------------//
+//----------------------------Level 1------------------------------//
+//-----------------------------------------------------------------//
+
+//-------Single Precision-------//
+
 //asum
 float
 (sasum_)(int 			*N,
@@ -82,10 +88,10 @@ void
 
 //rotg
 void
-(srotg_)(float *A,
-		 float *B,
-		 float *C,
-		 float *S)
+(srotg_)(float 			*A,
+		 float 			*B,
+		 float 			*C,
+		 float 			*S)
 {
     rotg(*A,*B,*C,*S);
 }
@@ -115,6 +121,115 @@ void
 int
 (isamax_)(int 			*N,
           float			*X,
+    	  int 			*INCX)
+{
+    return iamax(*N,X,*INCX);
+}
+
+//------------------------------//
+
+//-------Double Precision-------//
+
+//asum
+double
+(dasum_)(int 			*N,
+		 const double 	*X,
+         int			*INCX)
+{
+    return asum<double>(*N,X,*INCX);
+}
+
+//axpy
+void
+(daxpy_)(int 			*N,
+		 const double 	*ALPHA,
+         const double	*X,
+         int 			*INCX,
+         double			*Y,
+         int			*INCY)
+{
+    axpy(*N,*ALPHA,X,*INCX,Y,*INCY);
+}
+
+//copy
+void
+(dcopy_)(int 			*N,
+         const double	*X,
+         int 			*INCX,
+         double			*Y,
+         int			*INCY)
+{
+    copy(*N,X,*INCX,Y,*INCY);
+}
+
+//dot
+double
+(ddot_)(int 			*N,
+        const double	*X,
+        int 			*INCX,
+        const double	*Y,
+        int				*INCY)
+{
+    return dot<double>(*N,X,*INCX,Y,*INCY);
+}
+
+//nrm2
+double
+(dnrm2_)(int 			*N,
+         const double	*X,
+         int 			*INCX)
+{
+    return nrm2<double>(*N,X,*INCX);
+}
+
+//rot
+void
+(drot_)(int 			*N,
+        double			*X,
+    	int 			*INCX,
+        double			*Y,
+        int				*INCY,
+        double			*C,
+        double			*S)
+{
+    rot(*N,X,*INCX,Y,*INCY,*C,*S);
+}
+
+//rotg
+void
+(drotg_)(double 		*A,
+		 double 		*B,
+		 double 		*C,
+		 double 		*S)
+{
+    rotg(*A,*B,*C,*S);
+}
+
+//scal
+void
+(dscal_)(int 			*N,
+		 double			*ALPHA,
+         double			*X,
+    	 int 			*INCX)
+{
+    scal(*N,*ALPHA,X,*INCX);
+}
+
+//swap
+void
+(dswap_)(int 			*N,
+        double			*X,
+    	int 			*INCX,
+        double			*Y,
+        int				*INCY)
+{
+    swap(*N,X,*INCX,Y,*INCY);
+}
+
+//i_amax
+int
+(idamax_)(int 			*N,
+          double		*X,
     	  int 			*INCX)
 {
     return iamax(*N,X,*INCX);
