f908e943e3bef204cfc689acb512286c35f885f0
Chris Davis <chrisdavis132@gmail.com>
2013-04-06 18:18:16 +0100

    Added spr & double precision test suite
    - spr implementation added + tested
    - dblat2 double precision test suite added and modified to accept input
    data file
    - export functions for double precision test suite
    - tested existing gemv & ger implementations with double test suite
    (*.SUMM contain test suite output)

--- test-suite/export_L2.cpp
+++ test-suite/export_L2.cpp
@@ -3,7 +3,19 @@
 //#include "../level2/gbmv.tcc"			<-- NA
 #include "../level2/gemv.tcc"
 #include "../level2/ger.tcc"
-
+//#include "../level2/sbmv.tcc"			<-- NA
+//#include "../level2/spmv.tcc"			<-- NA
+//#include "../level2/spr2.tcc"			<-- NA
+#include "../level2/spr.tcc"
+//#include "../level2/symv.tcc"			<-- NA
+//#include "../level2/syr2.tcc"			<-- NA
+//#include "../level2/syr.tcc"			<-- NA
+//#include "../level2/tbmv.tcc"			<-- NA
+//#include "../level2/tbsv.tcc"			<-- NA
+//#include "../level2/tpmv.tcc"			<-- NA
+//#include "../level2/tpsv.tcc"			<-- NA
+//#include "../level2/trmv.tcc"			<-- NA
+//#include "../level2/trsv.tcc"			<-- NA
 
 
 extern "C" {
@@ -30,7 +42,7 @@ void
 		 float			*Y,
 		 int			*INCY)
 {
-	//sgbmv(*TRANS,*M,*N,*KL,*KU,*ALPHA,A,*LDA,X,*INCX,*BETA,Y,*INCY);
+	//gbmv(*TRANS,*M,*N,*KL,*KU,*ALPHA,A,*LDA,X,*INCX,*BETA,Y,*INCY);
 }
 
 //sgemv
@@ -50,7 +62,7 @@ void
 	gemv(*TRANS,*M,*N,*ALPHA,A,*LDA,X,*INCX,*BETA,Y,*INCY);
 }
 
-//ger									<-- Not available
+//ger
 void
 (sger_)(int				*M,
 		int				*N,
@@ -94,7 +106,7 @@ void
 		 float			*Y,
 		 int			*INCY)
 {
-	//sspmv(*UPLO,*N,*ALPHA,AP,X,*INCX,*BETA,Y,*INCY);
+	//spmv(*UPLO,*N,*ALPHA,AP,X,*INCX,*BETA,Y,*INCY);
 }
 
 //spr2
@@ -120,7 +132,7 @@ void
 		int				*INCX,
 		float			*AP)
 {
-	//spr(*UPLO,*N,*ALPHA,X,*INCX,AP);
+	spr(*UPLO,*N,*ALPHA,X,*INCX,AP);
 }
 
 //symv
@@ -251,6 +263,245 @@ void
 	//trsv(*UPLO,*TRANS,*DIAG,*N,A,*LDA,X,*INCX);
 }
 
+
+//-------Double Precision-------//
+
+//dgbmv									<-- Not available
+void
+(dgbmv_)(char			*TRANS,
+		 int			*M,
+		 int			*N,
+		 int			*KL,
+		 int			*KU,
+		 const double	*ALPHA,
+		 const double	*A,
+		 int			*LDA,
+		 const double	*X,
+		 int			*INCX,
+		 const double	*BETA,
+		 double			*Y,
+		 int			*INCY)
+{
+	//gbmv(*TRANS,*M,*N,*KL,*KU,*ALPHA,A,*LDA,X,*INCX,*BETA,Y,*INCY);
+}
+
+//dgemv
+void
+(dgemv_)(char			*TRANS,
+		 int			*M,
+		 int			*N,
+		 const double	*ALPHA,
+		 const double	*A,
+		 int			*LDA,
+		 const double	*X,
+		 int			*INCX,
+		 const double	*BETA,
+		 double			*Y,
+		 int			*INCY)
+{
+	gemv(*TRANS,*M,*N,*ALPHA,A,*LDA,X,*INCX,*BETA,Y,*INCY);
+}
+
+//ger
+void
+(dger_)(int				*M,
+		int				*N,
+		const double	*ALPHA,
+		const double	*X,
+		int				*INCX,
+		const double	*Y,
+		int				*INCY,
+		double			*A,
+		int				*LDA)
+{
+	ger(*M,*N,*ALPHA,X,*INCX,Y,*INCY,A,*LDA);
+}
+
+//sbmv
+void
+(dsbmv_)(char			*UPLO,
+		 int			*N,
+		 int			*K,
+		 const double	*ALPHA,
+		 const double	*A,
+		 int			*LDA,
+		 const double	*X,
+		 int			*INCX,
+		 const double	*BETA,
+		 double			*Y,
+		 int			*INCY)
+{
+	//sbmv(*UPLO,*N,*K,ALPHA,*LDA,X,*INCX,*BETA,Y,*INCY);
+}
+
+//spmv
+void
+(dspmv_)(char			*UPLO,
+		 int			*N,
+		 const double	*ALPHA,
+		 const double	*AP,
+		 const double	*X,
+		 int			*INCX,
+		 const double	*BETA,
+		 double			*Y,
+		 int			*INCY)
+{
+	//spmv(*UPLO,*N,*ALPHA,AP,X,*INCX,*BETA,Y,*INCY);
+}
+
+//spr2
+void
+(dspr2_)(char			*UPLO,
+		 int			*N,
+		 const double	*ALPHA,
+		 const double	*X,
+		 int			*INCX,
+		 const double	*Y,
+		 int			*INCY,
+		 double			*AP)
+{
+	//spr2(*UPLO,*N,*ALPHA,X,*INCX,Y,*INCY,AP);
+}
+
+//spr
+void
+(dspr_)(char			*UPLO,
+		int				*N,
+		const double	*ALPHA,
+		const double	*X,
+		int				*INCX,
+		double			*AP)
+{
+	spr(*UPLO,*N,*ALPHA,X,*INCX,AP);
+}
+
+//symv
+void
+(dsymv_)(char			*UPLO,
+		 int			*N,
+		 const double	*ALPHA,
+		 const double	*A,
+		 int			*LDA,
+		 const double	*X,
+		 int			*INCX,
+		 const double	*BETA,
+		 double			*Y,
+		 int			*INCY)
+{
+	//symv(*UPLO,*N,*ALPHA,A,*LDA,X,*INCX,*BETA,Y,*INCY);
+}
+
+//syr2
+void
+(dsyr2_)(char			*UPLO,
+		 int			*N,
+		 const double	*ALPHA,
+		 const double	*X,
+		 int			*INCX,
+		 const double	*Y,
+		 int			*INCY,
+		 double			*A,
+		 int			*LDA)
+{
+	//syr2(*UPLO,*N,*ALPHA,X,*INCX,Y,*INCY,A,*LDA);
+}
+		 
+//syr
+void
+(dsyr_)(char			*UPLO,
+		int				*N,
+		const double	*ALPHA,
+		const double	*X,
+		int				*INCX,
+		double			*A,
+		int				*LDA)
+{
+	//syr(*UPLO,*N,*ALPHA,X,*INCX,A,*LDA);
+}
+
+//tbmv
+void
+(dtbmv_)(char			*UPLO,
+		 char			*TRANS,
+		 char			*DIAG,
+		 int			*N,
+		 int			*K,
+		 const double	*A,
+		 int			*LDA,
+		 double			*X,
+		 int			*INCX)
+{
+	//tbmv(*UPLO,*TRANS,*DIAG,*N,*K,A,*LDA,X,*INCX);
+}
+
+//tbsv
+void
+(dtbsv_)(char			*UPLO,
+		 char			*TRANS,
+		 char			*DIAG,
+		 int			*N,
+		 int			*K,
+		 const double	*A,
+		 int			*LDA,
+		 double			*X,
+		 int			*INCX)
+{
+	//tbsv(*UPLO,*TRANS,*DIAG,*N,*K,A,*LDA,X,*INCX);
+}
+
+//tpmv
+void
+(dtpmv_)(char			*UPLO,
+		 char			*TRANS,
+		 char			*DIAG,
+		 int			*N,
+		 const double	*AP,
+		 double			*X,
+		 int			*INCX)
+{
+	//tpmv(*UPLO,*TRANS,*DIAG,*N,AP,X,*INCX);
+}
+
+//tpsv
+void
+(dtpsv_)(char			*UPLO,
+		 char			*TRANS,
+		 char			*DIAG,
+		 int			*N,
+		 const double	*AP,
+		 double			*X,
+		 int			*INCX)
+{
+	//tpsv(*UPLO,*TRANS,*DIAG,*N,AP,X,*INCX);
+}
+
+//trmv
+void
+(dtrmv_)(char			*UPLO,
+		 char			*TRANS,
+		 char			*DIAG,
+		 int			*N,
+		 const double	*A,
+		 int			*LDA,
+		 double			*X,
+		 int			*INCX)
+{
+	//trmv(*UPLO,*TRANS,*DIAG,*N,A,*LDA,X,*INCX);
+}
+
+//trsv
+void
+(dtrsv_)(char			*UPLO,
+		 char			*TRANS,
+		 char			*DIAG,
+		 int			*N,
+		 const double	*A,
+		 int			*LDA,
+		 double			*X,
+		 int			*INCX)
+{
+	//trsv(*UPLO,*TRANS,*DIAG,*N,A,*LDA,X,*INCX);
+}
 
 
 
